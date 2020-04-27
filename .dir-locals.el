23a18f3d4b486c026b193c86bdec0d0f55192b44
CreRecombinase <nwknoblauch@gmail.com>
2020-04-27 13:01:37 -0500

    not automatic commit

---
+++ .dir-locals.el
@@ -0,0 +1,22 @@
+((org-mode . ((org-publish-project-alist .
+                                         (
+                                          ("dissertation-org" . (:base-directory "/home/nwknoblauch/Dropbox/Repos/dissertation/org"
+                                                                                 :publishing-directory "/home/nwknoblauch/Dropbox/Repos/dissertation/docs"
+                                                                                 :publishing-function org-html-publish-to-html
+                                                                                 :makeindex t
+                                                                                 :auto-sitemap t
+                                                                                 :exclude "setup.org"
+                                                                                 ))
+                                          ("dissertation-fig" . (:base-directory "/home/nwknoblauch/Dropbox/Repos/dissertation/org"
+                                                                                 :publishing-directory "/home/nwknoblauch/Dropbox/Repos/dissertation/docs"
+                                                                                 :base-extension "png\\|\\|svg"
+                                                                                 :publishing-function org-publish-attachment
+                                                                                 :recursive t
+                                                                                 ))
+                                          ("dissertation" . (:components ("dissertation-org" "dissertation-fig") )))
+                                         )
+              (reftex-default-bibliography '("~/Dropbox/Repos/dissertation/bibliography/references.bib"))
+              (org-ref-bibliography-notes "~/Dropbox/Repos/dissertation/bibliography/notes.org")
+              (org-ref-pdf-directory "~/Dropbox/Repos/dissertation/bibliography/bibtex-pdfs/")
+
+              )))
